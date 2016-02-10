1b8a3cee883471e87eb8beb7944d6dcff0eaa9d7
eostpfi <mbesdeberc@unistra.fr>
2016-02-10 11:48:15 +0100

    Modifying default psd parameters for more suitable values

--- crossCalib.py
+++ crossCalib.py
@@ -43,12 +43,12 @@ def crossCalib(monitor_trace, response_trace, **kwargs):
 	if 'nfft' in kwargs:
 		n_fft=kwargs['nfft']
 	else:
-		n_fft=8192
+		n_fft=1024
 
 	if 'npad' in kwargs:
 		n_pad=kwargs['npad']
 	else:
-		n_pad=32768
+		n_pad=n_fft*4
 
 	if 'noverlap' in kwargs:
 		n_overlap=kwargs['noverlap']
