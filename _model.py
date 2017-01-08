508d7ae5607836c1ee48c775476576cb310519e6
mbdb <mbesdeberc@unistra.fr>
2017-01-08 18:34:33 +0100

    Normalize to pep8 coding style

--- _model.py
+++ _model.py
@@ -16,15 +16,17 @@ import scipy as sp
 
 
 def _model(f, p, h, a):
-	w0 = 2*np.pi/p
-	zeroes=np.array([0,0])
-	poles=np.array([w0*(-h+np.sqrt(1-h**2)*1j), w0*(-h-np.sqrt(1-h**2)*1j)])
-	(n, d)=sp.signal.zpk2tf(zeroes, poles, a)
-	(_w, h)=sp.signal.freqs(n, d, worN=2*np.pi*f)
-	return h
+    w0 = 2*np.pi/p
+    zeroes = np.array([0, 0])
+    poles = np.array([w0*(-h+np.sqrt(1-h**2)*1j), w0*(-h-np.sqrt(1-h**2)*1j)])
+    (n, d) = sp.signal.zpk2tf(zeroes, poles, a)
+    (_w, h) = sp.signal.freqs(n, d, worN=2*np.pi*f)
+    return h
+
 
 def modelPhase(f, p, h, a):
-	return np.angle(_model(f, p, h, a),deg=True)
+    return np.angle(_model(f, p, h, a), deg=True)
+
 
 def modelAmp(f, p, h, a):
-	return np.absolute(_model(f, p, h, a))
+    return np.absolute(_model(f, p, h, a))
--- sleeman.py
+++ sleeman.py
@@ -18,10 +18,12 @@ import numpy as np
 from matplotlib import mlab
 from obspy.signal.invsim import cosine_taper
 
+
 def detrend_func(data):
     data = mlab.detrend_mean(data)
     data = mlab.detrend_linear(data)
     return data
+
 
 def sleeman(stream):
     """
@@ -29,73 +31,84 @@ def sleeman(stream):
     :type stream: Stream object from module obspy.core.stream
     :param stream: Stream containing 3 traces with the same signal recorded.
     """
-
-    #Copy original stream
+    # Copy original stream
     m = stream.copy()
 
-    #Verify stream
+    # Verify stream
     if m[0].stats.sampling_rate != m[1].stats.sampling_rate \
-    or m[0].stats.sampling_rate != m[2].stats.sampling_rate \
-    or m[1].stats.sampling_rate != m[2].stats.sampling_rate:
+       or m[0].stats.sampling_rate != m[2].stats.sampling_rate \
+       or m[1].stats.sampling_rate != m[2].stats.sampling_rate:
         print("[pyColocSensors.sleeman]: Sampling rates are not identical \
         between traces")
 
     if m[0].stats.npts != m[1].stats.npts \
-    or m[0].stats.npts != m[2].stats.npts \
-    or m[1].stats.npts != m[2].stats.npts:
+       or m[0].stats.npts != m[2].stats.npts \
+       or m[1].stats.npts != m[2].stats.npts:
         print("[pyColocSensors.sleeman]: Traces does not have the same length")
 
-    if m[0].stats.starttime-m[1].stats.starttime  >= m[0].stats.sampling_rate/2\
-    or m[1].stats.starttime-m[2].stats.starttime  >= m[1].stats.sampling_rate/2\
-    or m[0].stats.starttime-m[2].stats.starttime  >= m[0].stats.sampling_rate/2:\
-        print("[pyColocSensors.sleeman]: Traces does not have the same start time")
+    if m[0].stats.starttime-m[1].stats.starttime >= m[0].stats.sampling_rate/2\
+       or m[1].stats.starttime-m[2].stats.starttime >= m[1].stats.sampling_rate/2\
+       or m[0].stats.starttime-m[2].stats.starttime >= m[0].stats.sampling_rate/2:
+        print("[pyColocSensors.sleeman]: Traces does not have the same start\
+              time")
 
-    #Set psd and csd parameters. Set as McNamara recommands in his paper, \
-    #except for windows length, staticaly fixed to 1024, to improve resolution
-    #(doi: 10.1785/012003001)
+    # Set psd and csd parameters. Set as McNamara recommands in his paper, \
+    # except for windows length, staticaly fixed to 1024, to improve resolution
+    # (doi: 10.1785/012003001)
     n_fft = 1024
     n_overlap = int(n_fft*0.75)
     fs = m[0].stats.sampling_rate
 
-    #Calculate psd and csd
-    (P00,f) = mlab.psd(m[0].data,Fs=fs,NFFT=n_fft,noverlap=n_overlap,\
-    detrend=detrend_func,window=cosine_taper(n_fft,p=0.2),scale_by_freq=True)
+    # Calculate psd and csd
+    (P00, f) = mlab.psd(m[0].data, Fs=fs, NFFT=n_fft, noverlap=n_overlap,
+                        detrend=detrend_func,
+                        window=cosine_taper(n_fft, p=0.2),
+                        scale_by_freq=True)
 
-    (P11,f) = mlab.psd(m[1].data,Fs=fs,NFFT=n_fft,noverlap=n_overlap,\
-    detrend=detrend_func,window=cosine_taper(n_fft,p=0.2),scale_by_freq=True)
+    (P11, f) = mlab.psd(m[1].data, Fs=fs, NFFT=n_fft, noverlap=n_overlap,
+                        detrend=detrend_func,
+                        window=cosine_taper(n_fft, p=0.2),
+                        scale_by_freq=True)
 
-    (P22,f) = mlab.psd(m[2].data,Fs =fs,NFFT=n_fft,noverlap=n_overlap,\
-    detrend=detrend_func,window=cosine_taper(n_fft,p=0.2),scale_by_freq=True)
+    (P22, f) = mlab.psd(m[2].data, Fs=fs, NFFT=n_fft, noverlap=n_overlap,
+                        detrend=detrend_func,
+                        window=cosine_taper(n_fft, p=0.2),
+                        scale_by_freq=True)
 
-    (P01,f) = mlab.csd(m[0].data,m[1].data, Fs=fs,NFFT=n_fft,noverlap=n_overlap,\
-    detrend=detrend_func,window=cosine_taper(n_fft,p=0.2),scale_by_freq=True)
+    (P01, f) = mlab.csd(m[0].data, m[1].data, Fs=fs, NFFT=n_fft,
+                        noverlap=n_overlap, detrend=detrend_func,
+                        window=cosine_taper(n_fft, p=0.2),
+                        scale_by_freq=True)
 
-    (P02,f) = mlab.csd(m[0].data,m[2].data, Fs=fs,NFFT=n_fft,noverlap=n_overlap,\
-    detrend=detrend_func,window=cosine_taper(n_fft,p=0.2),scale_by_freq=True)
+    (P02, f) = mlab.csd(m[0].data, m[2].data, Fs=fs, NFFT=n_fft,
+                        noverlap=n_overlap, detrend=detrend_func,
+                        window=cosine_taper(n_fft, p=0.2),
+                        scale_by_freq=True)
 
     P10 = np.conj(P01)
 
-    (P12,f) = mlab.csd(m[1].data,m[2].data, Fs=fs,NFFT=n_fft,noverlap=n_overlap,\
-    detrend=detrend_func,window=cosine_taper(n_fft,p=0.2),scale_by_freq=True)
+    (P12, f) = mlab.csd(m[1].data, m[2].data, Fs=fs, NFFT=n_fft,
+                        noverlap=n_overlap, detrend=detrend_func,
+                        window=cosine_taper(n_fft, p=0.2), scale_by_freq=True)
 
     P20 = np.conj(P02)
 
     P21 = np.conj(P12)
 
-    #Apply corrections as McNamara recommends
-    for spectra in [P00,P11,P22,P01,P02,P10,P12,P20,P21]:
+    # Apply corrections as McNamara recommends
+    for spectra in [P00, P11, P22, P01, P02, P10, P12, P20, P21]:
         spectra = spectra*1.142857
 
-    #Calculate electronic noises acoording to Sleeman's method
+    # Calculate electronic noises acoording to Sleeman's method
     N0 = P00-P10*P02/P12
     N1 = P11-P21*P10/P20
     N2 = P22-P02*P21/P01
 
-    #Remove first samples (3%) in order to avoid poor resolution problems.
-    #Samples remaining should be enough to cover one decade in frequency.
+    # Remove first samples (3%) in order to avoid poor resolution problems.
+    # Samples remaining should be enough to cover one decade in frequency.
     N0 = N0[int(n_fft*0.03):]
     N1 = N1[int(n_fft*0.03):]
     N2 = N2[int(n_fft*0.03):]
     f = f[int(n_fft*0.03):]
 
-    return (N0,N1,N2,f)
+    return (N0, N1, N2, f)
